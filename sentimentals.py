b92943433aa94336c7fb25a2c67f3a80141fdf60
Liberbell <liberbell@gmail.com>
2019-10-28 08:23:58 +0900

    sentimentals5

--- sentimentals.py
+++ sentimentals.py
@@ -3,3 +3,7 @@ from textblob import TextBlob
 with open('Movie-Reviews.txt', 'r') as fh:
     reviews = fh.readlines()
 print(reviews[:2])
+
+for review in reviews:
+    sentiment = TextBlob(review)
+    print('{:40} : {: 01.2f} : {:01.2f}'.format(review[:40]), sentiment.polarity, sentiment.subjectivity)
