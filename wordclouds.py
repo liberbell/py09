4553c328e87f419cc5a7c9ebc75e4c63795d486d
Liberbell <liberbell@gmail.com>
2019-10-28 08:09:13 +0900

    wordclouds6

--- wordclouds.py
+++ wordclouds.py
@@ -9,6 +9,13 @@ print('File data sample: ', filedata[:200])
 stopwords = set(STOPWORDS)
 wordcloud = WordCloud(stopwords=stopwords, max_words=25, background_color='white').generate(filedata)
 
+# mpLib.imshow(wordcloud)
+# mpLib.axis('off')
+# mpLib.show()
+
+stopwords.update(['many', 'using', 'want', 'value'])
+wordcloud = WordCloud(stopwords=stopwords, max_words=10, background_color='azure').generate(filedata)
+
 mpLib.imshow(wordcloud)
 mpLib.axis('off')
 mpLib.show()
