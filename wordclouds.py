4038c7077d303245c802f5de2a988ee68caed7c1
Liberbell <liberbell@gmail.com>
2019-10-27 14:29:21 +0900

    wordcloud2

--- wordclouds.py
+++ wordclouds.py
@@ -1,4 +1,7 @@
+from wordcloud import WordCloud, STOPWORD
 with open('Course-Descriptions.txt', 'r') as fh:
     filedata = fh.read()
 
 print('File data sample: ', filedata[:200])
+
+stopwords = set(STOPWORDS)
