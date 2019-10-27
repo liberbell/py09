b1ca1ca409aa856d610a7f47c4130be8ef976c08
Liberbell <liberbell@gmail.com>
2019-10-28 08:24:46 +0900

    sentimentals6

--- sentimentals.py
+++ sentimentals.py
@@ -6,4 +6,4 @@ print(reviews[:2])
 
 for review in reviews:
     sentiment = TextBlob(review)
-    print('{:40} : {: 01.2f} : {:01.2f}'.format(review[:40]), sentiment.polarity, sentiment.subjectivity)
+    print('{:40} : {: 01.2f} : {:01.2f}'.format(review[:40], sentiment.polarity, sentiment.subjectivity))
