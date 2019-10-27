c2f2c489b55ca2dbd674d5e0685ee20b96a6f05d
Liberbell <liberbell@gmail.com>
2019-10-28 08:05:28 +0900

    wordclouds5

--- wordclouds.py
+++ wordclouds.py
@@ -1,4 +1,5 @@
 from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as mpLib
 
 with open('Course-Descriptions.txt', 'r') as fh:
     filedata = fh.read()
@@ -7,3 +8,7 @@ print('File data sample: ', filedata[:200])
 
 stopwords = set(STOPWORDS)
 wordcloud = WordCloud(stopwords=stopwords, max_words=25, background_color='white').generate(filedata)
+
+mpLib.imshow(wordcloud)
+mpLib.axis('off')
+mpLib.show()
