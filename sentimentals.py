6e32dc9c88f61a4e8a8eda91a06974dc298b7f42
Liberbell <liberbell@gmail.com>
2019-10-29 07:41:13 +0900

    sentimentals2

--- sentimentals.py
+++ sentimentals.py
@@ -7,3 +7,14 @@ with open('Movie-Reviews.txt', 'r') as fh:
 for review in reviews:
     sentiment = TextBlob(review)
     print('{:40} : {: 01.2f} : {:01.2f}'.format(review[:40], sentiment.polarity, sentiment.subjectivity))
+
+labels = ['Negative', 'Neautral', 'Positive']
+vales = [0, 0, 0]
+
+for review in reviews:
+    sentiment = TextBlob(review)
+    polarity = round(( sentiment.polarity + 1) * 3) % 3
+
+    values[polarity] = values[polarity] + 1
+
+print('Final summarized count: ', values)
