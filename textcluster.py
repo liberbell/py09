590c5e5483913753eb7c5c212e3791612093a266
Liberbell <liberbell@gmail.com>
2019-10-29 08:01:05 +0900

    textcluster5

--- textcluster.py
+++ textcluster.py
@@ -3,3 +3,6 @@ import pandas as pd
 hashtags_df = pd.read_csv('Course-Hashtags.csv')
 print('Sample Hashtag Data: ')
 print(hashtags_df[:2])
+
+hash_list = hashtags_df['HashTags'].tolist()
+title_list = hashtags_df['Course'].tolist()
