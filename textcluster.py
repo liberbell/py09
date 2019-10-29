6a21476cf4859368b7e38ca88ae763d281ebba4c
Liberbell <liberbell@gmail.com>
2019-10-30 07:41:56 +0900

    textcluster3

--- textcluster.py
+++ textcluster.py
@@ -9,3 +9,4 @@ hash_list = hashtags_df['HashTags'].tolist()
 title_list = hashtags_df['Course'].tolist()
 
 vectorizer = TfidfVectorizer(stop_words='english')
+hash_matrix = vectorizer.fit_transform(hash_list)
