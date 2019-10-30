63e1201f7d734424f1882c5c4fdd81eb55c10f7e
Liberbell <liberbell@gmail.com>
2019-10-31 07:48:31 +0900

    classifications6

--- classifications.py
+++ classifications.py
@@ -1,8 +1,12 @@
 import nltk
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 with open('Course-Descriptions.txt', 'r') as fh:
     descriptions = fh.read().splitlines()
 print('Sample course descriptions: ', descriptions[:2])
 
-nltk.download('stopwords')
+# nltk.download('stopwords')
+nltk.download('wordnet')
+
+lemmatizer = WordNetLemmatizer()
