524d65768aa1c09d26968c83a98e8a4a45927986
Liberbell <liberbell@gmail.com>
2019-10-31 07:52:44 +0900

    classifications6

--- classifications.py
+++ classifications.py
@@ -1,12 +1,19 @@
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 with open('Course-Descriptions.txt', 'r') as fh:
     descriptions = fh.read().splitlines()
 print('Sample course descriptions: ', descriptions[:2])
 
 # nltk.download('stopwords')
-nltk.download('wordnet')
+# nltk.download('wordnet')
 
 lemmatizer = WordNetLemmatizer()
+
+def customtokenize(str):
+    tokens = nltk.word_tokenize(str)
+    nostop = list(filter(lambda token: token not in stopwords.words('english'), tokens))
+    lemmatized = [lemmatizer.lemmatize(word) for word in nostop ]
+    return lemmatized
