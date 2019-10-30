c66cd1d72c5576ef3d38784e4a03cb62b707a910
Liberbell <liberbell@gmail.com>
2019-10-31 07:46:03 +0900

    classifications4

--- classifications.py
+++ classifications.py
@@ -1,5 +1,8 @@
-
+import nltk
+from nltk.corpus import stopwords
 
 with open('Course-Descriptions.txt', 'r') as fh:
     descriptions = fh.read().splitlines()
 print('Sample course descriptions: ', descriptions[:2])
+
+nttk.download('stopwords')
