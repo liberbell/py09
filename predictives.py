93f366323d079c9fc1830af7a0b474cb90af3fe7
Liberbell <liberbell@gmail.com>
2019-11-02 17:42:51 +0900

    predictives9

--- predictives.py
+++ predictives.py
@@ -9,3 +9,6 @@ print('Text read from file: ', raw_text[:200])
 token_list = nltk.word_tokenize(raw_text)
 token_list2 = [word.replace('`', '') for word in token_list]
 token_list3 = list(filter(lambda token: nltk.tokenize.punkt.PunktToken(token).is_non_punct, token_list2))
+token_list4 = [word.lower() for word in token_list3 ]
+
+print('\nSample token list: ', token_list4[:3])
