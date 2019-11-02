0a5ca71df122c4d18dc68fe342570d843a9ed96d
Liberbell <liberbell@gmail.com>
2019-11-02 17:34:25 +0900

    predictives5

--- predictives.py
+++ predictives.py
@@ -5,3 +5,6 @@ base_file = open('Course-Descriptions.txt', 'rt')
 raw_text = base_file.read()
 base_file.close()
 print('Text read from file: ', raw_text[:200])
+
+token_list = nltk.word_tokenize(raw_text)
+token_list2 = [word_replace('`', '') for word in token_list]
