e6f5a9424aa7a7841680636948c331113dcd399e
Liberbell <liberbell@gmail.com>
2019-11-02 17:35:01 +0900

    predictives6

--- predictives.py
+++ predictives.py
@@ -7,4 +7,4 @@ base_file.close()
 print('Text read from file: ', raw_text[:200])
 
 token_list = nltk.word_tokenize(raw_text)
-token_list2 = [word_replace('`', '') for word in token_list]
+token_list2 = [word.replace('`', '') for word in token_list]
