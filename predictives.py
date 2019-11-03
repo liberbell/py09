dbcdd192f36b912b8166882e80d903cf34305a0e
Liberbell <liberbell@gmail.com>
2019-11-03 12:44:27 +0900

    predictives1

--- predictives.py
+++ predictives.py
@@ -13,6 +13,6 @@ token_list2 = [word.replace('`', '') for word in token_list]
 token_list3 = list(filter(lambda token: nltk.tokenize.punkt.PunktToken(token).is_non_punct, token_list2))
 token_list4 = [word.lower() for word in token_list3 ]
 
-print('\nSample token list: ', token_list4[:3])
+print('\nSample token list: ', token_list4[:10])
 print('\nTotal tokens: ', len(token_list4))
 conn = sqlite3.connect(':memory:')
